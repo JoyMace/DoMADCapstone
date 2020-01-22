c9f7dd56df7338cfd0050bdda1e3df4afbfb597c
Christoph98 <christoph.uhl@colorado.edu>
2020-01-21 18:35:57 -0700

    Added Christoph_backend branch

--- code/index.js
+++ code/index.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/rand', function (req, res) {
+	res.sendFile(dirname__ + "html/test.html")
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
