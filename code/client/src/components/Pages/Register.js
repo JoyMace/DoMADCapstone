d3994442cb569002d53fc35048b326c006dd7b58
sbkeupp <43832329+sbkeupp@users.noreply.github.com>
2020-02-05 17:36:03 -0700

    Add files via upload

---
+++ code/client/src/components/Pages/Register.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Register.css';
+
+function Register() {
+    return (
+        <div className="register">
+            <h1>Register</h1>
+            <p>This is the register page.</p>
+        </div>
+    )
+}
+
+export default Register;
