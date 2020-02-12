b348541175d14efaa817c1f86bf77f7350a3ed37
Joy Mace <Jocelyn.Mace@colorado.edu>
2020-02-12 15:39:48 -0700

    Profile Page basic
    Profile Page basic

--- code/client/src/components/Pages/Account.js
+++ code/client/src/components/Pages/Account.js
@@ -6,6 +6,18 @@ function Account() {
         <div className="account">
             <h1>Account</h1>
             <p>This is the account page.</p>
+			<div class='row'>
+				<div class='column'>
+					<div class='blue-column'>
+					This is where you add your trip
+				    </div>
+			    </div>
+            <div class='column'>
+                <div class='green-column'>
+                    This side will have the posts, newest first
+                </div>
+            </div>
+            </div>
         </div>
     )
 }
