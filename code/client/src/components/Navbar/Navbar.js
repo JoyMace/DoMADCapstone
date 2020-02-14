af770f7e622ca2e52c933c94789244f99a8817f2
sbkeupp <43832329+sbkeupp@users.noreply.github.com>
2020-02-14 13:55:28 -0700

    Update navbar to match master

--- code/client/src/components/Navbar/Navbar.js
+++ code/client/src/components/Navbar/Navbar.js
@@ -11,20 +11,16 @@ const navbar = props => (
                 <DrawerToggleButton click={props.drawerClickHandler} />
             </div>
             <div className="navbar_logo"><a href="/"><img src={logo} alt="" style={{height:'50px'}}/></a></div>
-            <div className="navbar_navigation_items_left">
+            <div className="navbar_navigation_items">
                 <ul>
                     <li className="left-items"><a href="/search_locations">Explore</a></li>
                     <div className="separator" />
                     <li className="left-items"><a href="/donate">Donate</a></li>
                     <div className="separator" />
                     <li className="left-items"><a href="/blogs">Blogs</a></li>
-                </ul>
-            </div>
-            <div className="navbar_navigation_items_right">
-                <ul>
-                    <li className="right-items"><a href="/about">Info</a></li>
-                    <div className="separator" />
                     <li className="right-items"><a href="/account">Share Your Trip</a></li>
+                    <div className="separator" />
+                    <li className="right-items"><a href="/about">Info</a></li>
                     <div className="separator" />
                     <li className="right-items"><a href="/account">Profile</a></li>
                 </ul>
