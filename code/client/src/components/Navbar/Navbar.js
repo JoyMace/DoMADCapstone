1e193992e2382239b65e17ef4ed443c4ec8f50df
sbkeupp <43832329+sbkeupp@users.noreply.github.com>
2020-02-14 13:05:28 -0700

    Adding blog updates and installed react-icons

--- code/client/src/components/Navbar/Navbar.js
+++ code/client/src/components/Navbar/Navbar.js
@@ -11,16 +11,20 @@ const navbar = props => (
                 <DrawerToggleButton click={props.drawerClickHandler} />
             </div>
             <div className="navbar_logo"><a href="/"><img src={logo} alt="" style={{height:'50px'}}/></a></div>
-            <div className="navbar_navigation_items">
+            <div className="navbar_navigation_items_left">
                 <ul>
                     <li className="left-items"><a href="/search_locations">Explore</a></li>
                     <div className="separator" />
                     <li className="left-items"><a href="/donate">Donate</a></li>
                     <div className="separator" />
                     <li className="left-items"><a href="/blogs">Blogs</a></li>
-                    <li className="right-items"><a href="/account">Share Your Trip</a></li>
-                    <div className="separator" />
+                </ul>
+            </div>
+            <div className="navbar_navigation_items_right">
+                <ul>
                     <li className="right-items"><a href="/about">Info</a></li>
+                    <div className="separator" />
+                    <li className="right-items"><a href="/account">Share Your Trip</a></li>
                     <div className="separator" />
                     <li className="right-items"><a href="/account">Profile</a></li>
                 </ul>
