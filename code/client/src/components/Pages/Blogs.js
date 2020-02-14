ca8c83451936e7e22e1276c442e6273294ca5801
sbkeupp <43832329+sbkeupp@users.noreply.github.com>
2020-02-14 14:10:43 -0700

    Update Blogs.js

--- code/client/src/components/Pages/Blogs.js
+++ code/client/src/components/Pages/Blogs.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './Blogs.css';
-import blogimage from '../../images/boulder_image.jpg'
+import blogimage from '../../images/boulder_image.jpg';
+import { FaStar } from 'react-icons/fa';
+import { FaStarHalf } from 'react-icons/fa';
+import { IconContext } from "react-icons";
+import { FaCaretDown } from 'react-icons/fa';
+import { FaArrowRight } from 'react-icons/fa';
 
 function Blogs() {
     return (
@@ -9,12 +14,14 @@ function Blogs() {
                 <p>View Donation Stories by country or scroll down to see the most recent posts. All stories are sorted by country and then by date with most recent stories appearing first. Click on the name of a continent to see where DoMAD users have been!</p>
             </div>
             <div className="country-button-container">
-                <button className="country-buttons">Africa</button>
-                <button className="country-buttons">Asia</button>
-                <button className="country-buttons">Australia</button>
-                <button className="country-buttons">North America</button>
-                <button className="country-buttons">South America</button>
-                <button className="country-buttons">Europe</button>
+                <IconContext.Provider value={{ className: "global-class-name", style: { verticalAlign: "middle" } }}>
+                    <button className="country-buttons">Africa <FaCaretDown /></button>
+                    <button className="country-buttons">Asia <FaCaretDown /></button>
+                    <button className="country-buttons">Australia <FaCaretDown /></button>
+                    <button className="country-buttons">North America <FaCaretDown /></button>
+                    <button className="country-buttons">South America <FaCaretDown /></button>
+                    <button className="country-buttons">Europe <FaCaretDown /></button>
+                </IconContext.Provider>
             </div>
             <div className="blog-container">
                 <div className="blog-entry">
