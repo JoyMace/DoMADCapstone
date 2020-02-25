37d79116ebb8290a47060e4c16827cb275fcb0e2
Natnael Abraha <correct-natnael.abraha@colorado.edu>
2020-02-24 19:02:59 -0700

    changed directory for contact-us api from the user/msg to contact-us/msg

--- code/app.js
+++ code/app.js
@@ -38,7 +38,7 @@ app.use(passport.session());
 // connect routers
 app.use('/api/user/auth', require('./routers/user/auth'));
 app.use('/api/user/reset', require('./routers/user/reset'));
-app.use('/api/user/msg', require('./routers/contact_us/msg'));
+app.use('/api/contact-us/msg', require('./routers/contact_us/msg'));
 
 
 
