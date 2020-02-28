e4ab7be88ae729de5ecd992f6f2ab8a7df31b465
dual8794 <dual8794@colorado.edu>
2020-02-28 13:29:58 -0700

    Added text to how it works page

--- code/client/src/components/Pages/About.js
+++ code/client/src/components/Pages/About.js
@@ -28,28 +28,7 @@ function About() {
                 destination, it would help enhance the quality of life for a substantial number of people in less developed communities. 
             </p>
 
-            <hr className="about"></hr>
-
-            <h1 class="header1"> How It Works
-            </h1>
-            < div class="divl">
-                <p class="origin">After planning a trip, visit DoMAD.org before traveling to gather information on what items are needed for the area you are visiting.
-                     For example, if you are planning to go trekking in Peru, you would consult DoMAD before departing, enter your trekking location 
-                     and learn that tarps and rain ponchos are most needed items in that area.
-                </p>
-            </div>
-            <div class="divr">
-                <p class="origin">
-                Based on this data, pack a few extra items to be donated to local organizations or directly to the local community when you arrive.
-                </p>
-            </div>
-            <div class="divl">
-                <p class="origin">
-                Since DoMAD relies heavily on user feedback, visit DoMAD.org after your trip to provide feedback regarding your experience. 
-                This includes providing feedback on whether the donation item was benficial, recommending a different donation item, 
-                or providing other travelers with helpful information!
-                </p>
-            </div>
+            
 
             <hr className="about"></hr>
 
--- code/client/src/components/Pages/HowItWorks.js
+++ code/client/src/components/Pages/HowItWorks.js
@@ -4,8 +4,30 @@ import './HowItWorks.css';
 function HowItWorks() {
     return (
         <div className="how">
-            <h1>Donate</h1>
-            <p>This is the Donate page.</p>
+            <hr className="about"></hr>
+
+            <h1 class="header1"> How It Works</h1>
+        < div class="divl">
+            <p class="origin">After planning a trip, visit DoMAD.org before traveling to gather information on what items are needed for the area you are visiting.
+            For example, if you are planning to go trekking in Peru, you would consult DoMAD before departing, enter your trekking location 
+            and learn that tarps and rain ponchos are most needed items in that area.
+            </p>
+        </div>
+
+        <div class="divr">
+            <p class="origin">
+            Based on this data, pack a few extra items to be donated to local organizations or directly to the local community when you arrive.
+            </p>
+        </div>
+
+        <div class="divl">
+            <p class="origin">
+            Since DoMAD relies heavily on user feedback, visit DoMAD.org after your trip to provide feedback regarding your experience. 
+            This includes providing feedback on whether the donation item was benficial, recommending a different donation item, 
+            or providing other travelers with helpful information!
+            </p>
+        </div> 
+         
         </div>
     )
 }
