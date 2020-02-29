03440c2575899a9987cd9b633d0820c0f2a0db6e
sbkeupp <43832329+sbkeupp@users.noreply.github.com>
2020-02-29 16:40:21 -0700

    Add dropdown to navbar, add content to country page components

--- code/client/src/components/CountryPages/DonationItems.js
+++ code/client/src/components/CountryPages/DonationItems.js
@@ -4,7 +4,24 @@ import './DonationItems.css';
 function DonationItems() {
     return (
         <div className="donationitems">
-            
+            <div className="column1-donations">
+                <h4 className="category-header">Select a category for more info</h4>
+                <div className="donation-row">Animal Welfare</div>
+                <div className="donation-row">Art</div>
+                <div className="donation-row">Clothing</div>
+                <div className="donation-row">Education</div>
+                <div className="donation-row">Food</div>
+                <div className="donation-row">Health</div>
+                <div className="donation-row">Household</div>
+                <div className="donation-row">Miscellaneous</div>
+                <div className="donation-row">Sports</div>
+            </div>
+            <div className="column2-donations">
+
+            </div>
+            <div className="column3-donations">
+
+            </div>
         </div>
     )
 }
