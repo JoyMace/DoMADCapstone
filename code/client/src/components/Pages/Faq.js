e770d000a08a6b97eca5e480bd3c0556409d0806
Joy Mace <Jocelyn.Mace@colorado.edu>
2020-03-02 21:17:33 -0700

    Basic FAQ Page from Figma- needs links

--- code/client/src/components/Pages/Faq.js
+++ code/client/src/components/Pages/Faq.js
@@ -4,8 +4,52 @@ import './Faq.css';
 function Faq() {
     return (
         <div className="faq">
-            <h1>FAQ</h1>
-            <p>This is the FAQ page.</p>
+		<body>
+			
+            <h1>Frequently Asked Questions</h1>	
+			<br></br>
+			<section>
+            <p>Can’t find what you’re looking for? Contact us now..</p>
+			<br></br>
+			</section>
+			
+			<section>
+			<h2>How can I donate items?</h2>
+			<br></br>
+			<p> Your donation can be given directly to an individual or to a local organization or an actual DoMAD organization page.</p>
+			<p> For example, if you are going to a country in Sub-Saharan Africa, you could bring a mosquito net to donate upon arrival.
+			Alternatively, you could buy your donation item upon arrival at your destination to help support the local economy.</p>
+			</section>
+			<br></br>
+			<section>
+			<h2>What kinds of items can I donate?</h2>
+			<br></br>
+			<p>DoMAD provides suggested items for travelers to donate at their travel destination.</p>  
+			<p>In many parts of the developing world, items that are relatively inexpensive
+			   for most international travelers such as soap, bandages, flashlights, tarps and reading glasses 
+			are not readily available or affordable.</p>
+			
+			</section>
+			<br></br>
+			<section>
+			<h2>Why should I report a trip? </h2>
+			<br></br>
+			<p> By providing DoMAD with your donation details, we can see what is needed
+			    where and use this data to encourage more travelers to donate, 
+				plus provide easy ways for users to donate across the globe!
+			</p>
+			</section>
+			<br></br>
+			<section>
+			<h2>How can I contribute to DoMAD? </h2>
+			<br></br>
+			<p> DoMAD does not ask for (or accept) monetary contributions – 
+				we do ask you to share your travel experiences!</p>  
+			<p>We need your donation ideas and experiences to inspire other travelers to give back when they travel. 
+				Travelers are asked to rate their donation experience to help others identify most needed and appreciated items.
+			</p>
+			</section>
+		</body>
         </div>
     )
 }
