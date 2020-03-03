6b914264f5dfa35e66fc7e85a5d5ad4642af21ce
Joy Mace <Jocelyn.Mace@colorado.edu>
2020-03-02 18:53:10 -0700

    major form formatting

--- code/client/src/components/Pages/Account.js
+++ code/client/src/components/Pages/Account.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './Account.css';
 import WorldMapImage from '../../images/WorldMap.png';
 import avatar from '../../images/Avatar.png';
-import { FaStar } from 'react-icons/fa';
-import { FaStarHalf } from 'react-icons/fa';
-import { IconContext } from "react-icons";
- 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
+const userInfo = {
+	avatar: <img src={ avatar } alt="avatar" height='64px'/>,
+	name: 'Joy Mace',
+	memberSince: '1999',
+	hometown: 'Denver, CO',
+	totalDonations: '105',
+	totalContributions: '99',	
+};
 
 function UserInfo(props) {
   return (
@@ -21,67 +27,6 @@ function UserInfo(props) {
     </div>
   );
 }
-
-function Post(props) {
-	return (
-	<div className="Post"> 
-	  <div className="Post-date"> Date: {post.date}</div>
-	  <div className="Post-destination"> Destination: {post.destination}</div>
-	  <div className="Post-donation"> Items Donated:  {post.donation}</div>
-	  <div className="Post-stars"> Donation rating: {post.stars}</div>
-	  <div className="Post-description">Travel Story: {post.description}</div>
-	  
-	</div>
-  );
-}
-
-function Account(props) {
-  return (
-	<div className='row'>	
-      <div className='column'>
-		<div className='left-column'>
-		  <div className="Account">
-			  <UserInfo/>
-			  <div className="map-image">
-				<h1>Your Travel Map</h1>
-				<img src={ WorldMapImage } alt="World Map" />
-					<p style={{fontSize:12, lineHeight:2}}> Right click Your Travel Map at the location to drop a map pin there.</p>
-
-			  </div>
-			  <h1> Your Trips </h1>
-		  <Post/>
-			  
-		  </div>
-		</div>
-	  </div>
-	<div className='column'>
-      <div className='right-column'>	  
-        <h3>Share your recent DoMAD travel story!</h3>
-			  
-			  <UserDonationStory 
-				style={{
-				backgroundColor: 'blue',
-				width: '100px',
-				height: '100px'
-				}}
-				/>
-  		  
-
-      </div>
-    </div>
-	</div>
-
-    
-  );
-}
-const userInfo = {
-	avatar: <img src={ avatar } alt="avatar" height='64px'/>,
-	name: 'Joy Mace',
-	memberSince: '1999',
-	hometown: 'Denver, CO',
-	totalDonations: '105',
-	totalContributions: '99',	
-};
 
 class UserDonationStory extends React.Component {
 	constructor(props) {
@@ -106,71 +51,164 @@ class UserDonationStory extends React.Component {
 		const { post } = this.props
 		return (
 		
-		<form>
-		<div className='sub-entry'>
-			<p style={{lineHeight:2}}>When did this trip occur?</p>
-			<p style={{lineHeight:2}}>Where did you go?</p>
-			<p style={{lineHeight:2}}>What did you donate?</p>
-			<p style={{lineHeight:2}}>How was the donation received?</p>
-			<p style={{lineHeight:2}}>What else would you like to share?</p>
-			<p style={{lineHeight:2}}>Make private?</p>
-			<select name='public_private' value={this.state.public_private} onChange={this.accountChangeHandler}>
-			  <option value="Private">Private</option>
-			  <option value="Public">Public</option>
-			</select>	
-			<p style={{lineHeight:2}}>Upload Pictures?</p>
-			<input type='file' />
-		</div>
-		
-		<div className='sub-entry'>
-			<input name='date' type="date" onChange={this.accountChangeHandler } />
+		<form action="/api/user/trip/report" method="POST">
+			<ul class="flex-outer">
+				<li>
+					<label for="date">When did this trip occur?</label>
+					<input id="date" name='date' type="date" onChange={this.accountChangeHandler } />	
+				</li>
 				
-			<input type="text" />
-			
-			
-			<select name= 'donationRecipient' value={this.state.donationRecipient} onChange={this.accountChangeHandler}>
-			  <option value="Individual">Individual</option>
-			  <option value="Organization">Organization</option>
-			  <option value="Both">Both</option>
-			</select>	
-			
-			<select name='donationItem' value={this.state.donationItem} onChange={this.accountChangeHandler}>
-			  <option value="Tarp">Tarp</option>
-			  <option value="Medical Supplies">Medical Supplies</option>
-			  <option value="Toothbrush">Toothbrush</option>
-			  <option value="Solar Light">Solar Light</option>
-			  <option value="Supplements">Supplements</option>
-			  <option value="Books">Books</option>
-			  <option value="Toiletries">Toiletries</option>
-			  <option value="Water Purification">Water Purification</option>
-			</select>
-			
-			<select name= 'donationRecipient' value={this.state.donationRecipient} onChange={this.accountChangeHandler}>
-			  <option value="Individual">Individual</option>
-			  <option value="Organization">Organization</option>
-			  <option value="Both">Both</option>
-			</select>
-			
-			<IconContext.Provider name='rating' value={{ color: "yellow", className: "global-class-name", style: { verticalAlign: "middle" } }} onChange={this.accountChangeHandler}>
+				<li>
+					<label name="location" className="location">Where did you go?</label>
+					<input name="location" type="text" placeholder="Enter city, country" value={this.state.location} onchange={this.accountChangeHandler}/>
+				</li>
+				
+				<li>
+					<label name="donationItem" className="donationItem">What did you donate?</label>
+					<input name="donationItem" className="donationItem" type="text" placeholder="Enter Donation Item"/>
+				</li>
+				<li>
+					<label name="donationCategory" className="donationCategory"></label>
+					<select name='donationItem' value={this.state.donationItem} onChange={this.accountChangeHandler}>
+					  <option selected="selected">Select the Donation Category</option>
+					  <option value="Medical">Medical</option>
+					  <option value="Education">Education</option>
+					  <option value="WaterPurification">Water Purification</option>
+					</select>
+				</li>
+				<li>
+					<p>Did you donate to an Individual or Organization?</p>
+						<ul class="flex-inner">
+							<li>
+							<label for="donationRecipient" name="donationRecipient">Individual</label>
+							<input type="checkbox" id="Individual" name="donationRecipient" value={this.state.donationRecipient}/>
 							
-			 <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStarHalf />
+							</li>
+							<li>
+							<label for="donationRecipient" name="donationRecipient">Organization</label>
+							<input type="checkbox" id="ORganization"/>
 							
-			</IconContext.Provider>
+							</li>
+						
+						</ul>
+				</li>
+					
+				<li>	
+					<label name="rating" className="rating">How was the donation received?</label>
+						<div className='star-rating'>
+							<FontAwesomeIcon icon={faStar} />
+							<FontAwesomeIcon icon={faStar} />
+							<FontAwesomeIcon icon={faStar} />
+							<FontAwesomeIcon icon={faStar} />
+							<FontAwesomeIcon icon={faStar} />
+						</div>
+					
+				</li>
+				
+				<li>
+					<label name="donationItem" className="donationItem">Suggest Future Donation Item?</label>
+					<input name="donationItem" className="donationItem" type="text" placeholder="Enter Donation Item"/>
+				</li>
+				<li>
+					<label name="donationCategory" className="donationCategory"></label>
+					<select name='donationItem' value={this.state.donationItem} onChange={this.accountChangeHandler}>
+					  <option selected="selected">Select the Donation Category</option>
+					  <option value="Medical">Medical</option>
+					  <option value="Education">Education</option>
+					  <option value="WaterPurification">Water Purification</option>
+					</select>
+				</li>
+				<li>
+					<label name="donationReason" className="donationReason"></label>
+					<input name="location" type="text" placeholder="Enter Reason for Future Donation" value={this.state.location} onchange={this.accountChangeHandler}/>
+				</li>
+				<li>
+					<label name="description" className="description" >What else would you like to share?</label>
+					<input name='description' type="text" placeholder="Type your story here." onChange={this.accountChangeHandler}/>
+				</li>
+				
+				<li>
+					<p>Make Private?</p>
+						<ul class="flex-inner">
+						<li>
+						<label for="private" name="public_private" className="public_private">Private</label>
+						<input type="checkbox" id="private"/>
+						</li>
+						</ul>
+				</li>
+				
+				<li>
+					<label name="pictures" className="pictures">Upload Pictures?</label>
+					<input type='file' size="100"/>
+				</li>
 			
-			<input name='description' type="text" onChange={this.accountChangeHandler}/>
-		</div>	
-			<p><button className="SubmitButton">Submit</button></p>
+				<li>
+					<button type="submit">Submit</button>
+				</li>
+			</ul>			
 		</form>
-			);
+		);
 	}
-} 
+}
+
 const post = {
 	date: UserDonationStory.date, 
 	destination: UserDonationStory.destination,
 	donation: UserDonationStory.donationItem,
 	stars: UserDonationStory.rating,
 	description: UserDonationStory.description,
-};  
+};
+
+function Post(props) {
+	return (
+	<div className="Post"> 
+	  <div className="Post-date"> Date: {post.date}</div>
+	  <div className="Post-destination"> Destination: {post.destination}</div>
+	  <div className="Post-donation"> Items Donated:  {post.donation}</div>
+	  <div className="Post-stars"> Donation rating: {post.stars}</div>
+	  <div className="Post-description">Travel Story: {post.description}</div>
+	  
+	</div>
+  );
+}
+
+function Account(props) {
+  return (
+	<div className='row'>
+		<div className='column'>
+		<div className='left-column'>
+		  <div className="Account">
+			<UserInfo/>
+			  <div className="map-image">
+				<h1>Your Travel Map</h1>
+				<img src={ WorldMapImage } alt="World Map" />
+					<p style={{fontSize:12, lineHeight:2}}> Right click Your Travel Map at the location to drop a map pin there.</p>
+
+			  </div>
+			  <h1> Your Trips </h1>
+				<Post/>
+			  
+		  </div>
+		</div>
+	  </div>
+	<div className='column'>
+      <div className='right-column'>	  
+			  <div className='container'>
+			  <h3 style={{fontSize: 18, textAlign: "center", lineHeight: 5}}>Share your recent DoMAD travel story!</h3>
+			  <UserDonationStory />
+			  </div>
+
+      </div>
+    </div>
+	</div>
+
+    
+  );
+}
+
+
+ 
+  
 ReactDOM.render(
   <Account  />,
   document.getElementById('root')
