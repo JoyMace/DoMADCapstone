4205707a22428cf07d663b605c804300d0ab821e
sbkeupp <43832329+sbkeupp@users.noreply.github.com>
2020-03-06 13:19:46 -0700

    Add content to blogs

--- code/client/src/components/Pages/IndividualBlogs.js
+++ code/client/src/components/Pages/IndividualBlogs.js
@@ -4,7 +4,14 @@ import './IndividualBlogs.css';
 function IndividualBlogs() {
     return (
         <div className="individual-blogs">
-            
+            <div className="blog-country-name">
+                <h1>Country Name</h1>
+            </div>
+            <div className="blog-info-container">
+                <p>Date of travel: 01/20/2020</p>
+                <h2>Donations</h2>
+                <h2>Suggested Donations</h2>
+            </div>
         </div>
     )
 }
