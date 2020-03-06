7b7da4617b4127a62d50a24ce0f860bdb9f94623
dual8794 <dual8794@colorado.edu>
2020-03-06 13:50:57 -0700

    Added steps images to how page

--- code/client/src/components/Pages/HowItWorks.js
+++ code/client/src/components/Pages/HowItWorks.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import './HowItWorks.css';
 import Map from '../../images/Map.png'
+import HowItWorksSearchBar from '../../images/HowItWorksSearchBar.png'
+import Step1 from '../../images/Step1.png'
+import Step2 from '../../images/Step2.png'
+import Step3 from '../../images/Step3.png'
+
 
 function HowItWorks() {
     return (
         <div className="how">
-            <hr className="about"></hr>
 
-            <h1 class="header1"> How It Works</h1>
+            <h1 className="how"> How It Works</h1>
 
             <div class="rightImg">
                 <img class="howMap" src={Map} width="450" alt="Image"/>
             </div>
 
-            <h1 className="leftHeader">Gather Info </h1>
+            <div className="rightImg2">
+                <img class="howSearchBar" src={HowItWorksSearchBar} width="450" alt="Image"/>
+            </div>
 
-            < div class="divLeft">
-                <p class="origin"> &emsp; &emsp; &emsp; After planning a trip, visit DoMAD.org before traveling to gather information on what 
+            <div className="leftStep1">
+                <img className="step1" src={Step1} width="450" alt="Image"/>
+            </div>
+
+            < div class="divLeft">                
+                <p class="origin"> 
+                <h1>&emsp; &emsp;Gather Info</h1>
+                &emsp; &emsp; &emsp; After planning a trip, visit DoMAD.org before traveling to gather information on what 
                 items are needed for the area you are visiting. For example, if you are planning to go trekking in Peru, 
                 you would consult DoMAD before departing, enter your trekking location and learn that tarps and rain 
                 ponchos are most needed items in that area.
@@ -26,8 +38,13 @@ function HowItWorks() {
                 </p>
             </div>
 
+            <div className="rightStep2">
+                <img className="step2" src={Step2} width="450" alt="Image"/>
+            </div>
+
             <div class="divr">
-                <p class="origin">
+                <p class="origin"> 
+                <h1>&emsp; &emsp;Pack Items</h1>
                 &emsp; &emsp; &emsp; Based on this data, pack a few extra items to be donated to local organizations or directly to 
                 the local community when you arrive. Make sure to check with your destination’s embassy and your 
                 airline to avoid traveling with forbidden items. 
@@ -38,8 +55,13 @@ function HowItWorks() {
                 </p>
             </div>
 
+            <div className="leftStep3">
+                <img className="step3" src={Step3} width="450" alt="Image"/>
+            </div>
+
             <div class="divl">
                 <p class="origin">
+                <h1>&emsp; &emsp;Share your Trip</h1>
                 &emsp; &emsp; &emsp; Visit DoMAD.org after your trip to provide feedback regarding your experience.
                 This includes providing feedback on whether the donation item was benficial, 
                 recommending a different donation item, or providing other travelers with helpful information!
@@ -48,6 +70,12 @@ function HowItWorks() {
                 </p>
             </div> 
          
+            <div className="bluePadding">
+                <p className="downCenter" >If even a small percentage of the ~ 1.24 billion people who travel internationally each year donated
+                 a needed item at their destination, it would help enhance the quality of life for a substantial number of people.</p>
+
+            </div>
+            
         </div>
     )
 }
