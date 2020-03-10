d89c282fe2c7fb51d0bbab2e81bbd16d9d7e11b3
sbkeupp <43832329+sbkeupp@users.noreply.github.com>
2020-03-09 18:17:19 -0600

    Add design for blog pages, add disclaimer content, finish faq page, fix dropdowns for blog page.

--- code/client/src/components/Pages/IndividualBlogs.js
+++ code/client/src/components/Pages/IndividualBlogs.js
@@ -8,31 +8,35 @@ function IndividualBlogs() {
                 <h1>Country Name</h1>
             </div>
             <div className="blog-info-container">
-                <p>Date of travel: 01/20/2020</p>
+                <h3>Date of travel: 01/20/2020</h3>
+                <div className="travel-story">
+                    <h2>Travel Story</h2>
+                    <div className="travel-story-information">
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                    </div>
+                </div>
                 <div className="blog-donations">
                     <h2>Donations</h2>
                     <div className="blog-donation-items">
                         <div className="blog-donation-column">
                             <h5 className="donated-item-label">Donation Item</h5>
+                            <p>Tarp</p>
                         </div>
-                        <div className="blog-location-column">
-                            <h5>Individual or Organization?</h5>
+                        <div className="blog-donation-column">
+                            <h5 className="donated-item-label">Individual or Organization?</h5>
+                            <p>Individual</p>
                         </div>
-                        <div className="blog-rating-column">
-                            <h5>Item Rating</h5>
+                        <div className="blog-donation-column">
+                            <h5 className="donated-item-label">Item Rating</h5>
+                            <p>4/5</p>
                         </div>
                     </div>
                 </div>
                 <div className="blog-suggested-donations">
                     <h2>Suggested Donations</h2>
                     <div className="blog-suggested-donation-items">
-                        Suggested Donatation Items
-                    </div>
-                </div>
-                <div className="travel-story">
-                    <h2>Travel Story</h2>
-                    <div className="travel-story-information">
-                        Travel Story
+                        <p>Item: Tarp</p>
+                        <p>Reason for suggestion: They need it.</p>
                     </div>
                 </div>
             </div>
