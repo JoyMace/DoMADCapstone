5edd6b5b121da7425c237db62546949c75a51e26
Christoph98 <christoph.uhl@colorado.edu>
2020-03-11 17:22:32 -0600

    removed most comments from donation test

--- code/test/user.donation.test.js
+++ code/test/user.donation.test.js
@@ -4,10 +4,6 @@ const request = require('supertest');
 const sinon = require('sinon');
 const sandbox = sinon.createSandbox();
 
-/*
-  These will be included on a case-by-case basis
-*/
-const app = require('../app'); // used for supertest
 const donationCodes = require('../config/resCodes/user/donation');
 const testVar = require('../config/testVar'); // generic test donation
 const Donation = require('../models/donation');
@@ -19,18 +15,6 @@ describe('Donation functions', function() {
   describe('Create Donation Test', function() {
 
     before(function(done) {
-
-      /*
-        Create any stubs or variables you need for the tests
-      
-        example stub:
-        var functionStub = sandbox.stub([object to stub], [function to stub])
-        functionStub.yields - used to insert values into callback function 
-        functionStub.onCall(n).yields - onCall is used to change the behavior of the stub the nth time the functon is called ( see admin.test.js )
-        functionStub.withArgs([args]).yields - withArgs is uesed to cahnge the bahvior of the stub when called with specific arguments ( see user.models.test.js )
-      */
-
-
       var fakeDonation = new Donation(testVar.donationInformation);
       var mongooseSaveStub = sandbox.stub(Donation.prototype, 'save');
       //successful
@@ -40,27 +24,7 @@ describe('Donation functions', function() {
       done();
     });
 
-    //  Note there is a beforeEach function that is called before each test and not only at the start
-
-    /*
-      it - indavidual test
-      This is used to test different expected behaviors of a function
-
-      The way our tests are structured there are two types of tests:
-        - SUCCESS
-        - FAILURE 
-      There should be a test created for each of the different ways the function can succeed or fail.
-      When there are multiple ways it can succeed or fail add a small desciption in the it name.
-    */
     it('saveDonation - SUCCESS', function(done){
-
-      /*
-        1. any setup that wasn't handled in before or beforeEach
-        2. Call functions and record output
-        3. check if the output is what you expected
-          expect([output]).to.equal([expected output]) - check chai documentation for other asserts
-      */
-
       donationFunctions.createDonation(testVar.donationInformation, testVar.tripInfo._id, function(err, donation, res){
         statusCode = res.status;
         message = res.message;
@@ -85,9 +49,6 @@ describe('Donation functions', function() {
       done();
     }); 
 
-    /*
-      after is called at the end of a set of unit tests to clean up the testing environemnt
-    */
     after(function(done) {
       sandbox.restore();
       done();
