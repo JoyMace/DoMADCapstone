da3e4d8874ec71b8ee06713e2246dcec233ac8ed
sbkeupp <43832329+sbkeupp@users.noreply.github.com>
2020-03-13 14:44:07 -0600

    Style contact us page, add tabs to donations items, fix tabs styling on country page, add commented out code to blog for back end connection, add filler info for organizations, fix navbar and side drawer

--- code/client/src/components/Pages/Contact.js
+++ code/client/src/components/Pages/Contact.js
@@ -4,26 +4,35 @@ import './Contact.css';
 function Contact() {
 
     return (
-
         <div className="contact">
-            <form action="/api/contact-us/msg/contact" method="POST">
-                  First Name: <input type="text" name="firstname" />
-                  Last Name: <input type="text" name="lastname" /><br/>
-                  email: <input type="text" name="email" /><br/>
-
-                  <h4>Questions or Feedback</h4>
-                  <textarea rows="5" id="message" name="message"></textarea><br/>
-                  <button type="submit">Sumbit</button>
-            </form>
+            <h1 className="contact-us-header">Contact Us</h1>
+            <div className="contact-style">
+                <div className="contact-form-container">
+                    <form action="/api/contact-us/msg/contact" method="POST">
+                        <div className="first-last-name-contact">
+                            <div className="first-name-contact">
+                                <p>First Name:</p>
+                                <input type="text" name="firstname" className="input-contact"/>
+                            </div>
+                            <div className="last-name-contact">
+                                <p>Last Name:</p>
+                                <input type="text" name="lastname" className="input-contact"/><br/>
+                            </div>
+                        </div>
+                        <div className="email-contact">
+                            <p>Email:</p>
+                            <input type="text" name="email" className="input-contact"/><br/>
+                        </div>
+                        <div className="feedback-contact">
+                            <p>Questions or Feedback</p>
+                            <textarea rows="5" id="message" name="message" className="feedback-box-contact"></textarea><br/>
+                        </div>
+                        <button type="submit" className="submit-contact-button">Sumbit Form</button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
 
-
 export default Contact;
-
-
-// <div className="contact">
-//     <h1>Contact Us</h1>
-//     <p>This is the contact us page.</p>
-// </div>
