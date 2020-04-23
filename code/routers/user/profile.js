3caf64603d8dab13a87bc955bebe7bedd1d1477f
Joy Mace <Jocelyn.Mace@colorado.edu>
2020-04-22 19:09:08 -0600

    missed these files from last pull request

--- code/routers/user/profile.js
+++ code/routers/user/profile.js
@@ -21,7 +21,7 @@ const profileCodes = require('../../config/resCodes').profile;
 
 
 router.get('/profile', (req, res) => {
-
+console.log(req.user);
   var userID;
   // checks if user is logged in or external request
 
@@ -47,17 +47,17 @@ router.get('/profile', (req, res) => {
       userData = {userName: user.username, firstName: user.firstName, lastName: user.lastName,
                 signupDate: user.signupDate, locationID: user.locationID};
 
-      Trip.aggregate( [ { $group: { "_id" : userID, count: { $sum: 1 } } } ], function(err, user) {
-
+      Trip.aggregate( [ { $group: { "_id" : userID, count: { $sum : 1 } } } ], function(err, user) {
+        console.log(err);
         if(err) {
           return res.status(profileCodes.profile.tripcountNotFound.status).send({
             message: profileCodes.profile.tripcountNotFound.message
           });
         }
         else{
-          userData['tripsCount'] = user[0].count;
+          userData['tripsCount'] = user[0] ? user[0].count : 0
 
-          Donation.aggregate( [ { $group: {"_id" : userID, count: { $sum: 1 }}}], function(err, user) {
+          Donation.aggregate( [ { $group: {"_id" : userID, count: { $sum : 1 }}}], function(err, user) {
 
             if(err) {
               return res.status(profileCodes.profile.donationcountNotFound.status).send({
@@ -65,7 +65,7 @@ router.get('/profile', (req, res) => {
               });
             }
             else{
-              userData['donationCount'] = user[0].count;
+              userData['donationCount'] = user[0] ? user[0].count : 0
               return res.status(profileCodes.profile.success.status).send({userData: userData});
             }
           });
