99b57b85011a01fa5b040d76a6614695066cb61c
Joy Mace <Jocelyn.Mace@colorado.edu>
2020-04-30 17:25:35 -0600

    minor formatting changes to Contact Us Form to have consistent form styling on all pages

--- code/client/src/components/Pages/Contact.js
+++ code/client/src/components/Pages/Contact.js
@@ -5,27 +5,27 @@ function Contact() {
 
     return (
         <div className="contact">
-            <h1 className="contact-us-header">Contact Us</h1>
             <div className="contact-style">
                 <div className="contact-form-container">
+                    <div className='contact-title'style={{fontSize: '36px'}}>Contact Us</div>
                     <form action="/api/contact-us/msg/contact" method="POST">
                         <div className="first-last-name-contact">
                             <div className="first-name-contact">
                                 <p>First Name:</p>
-                                <input required = "Required" type="text" name="firstname" className="input-contact"/>
+                                <input required = "Required" type="text" name="firstname" className="input-contact" placeholder='First Name'/>
                             </div>
                             <div className="last-name-contact">
                                 <p>Last Name:</p>
-                                <input required = "Required" type="text" name="lastname" className="input-contact"/><br/>
+                                <input required = "Required" type="text" name="lastname" className="input-contact" placeholder='Last Name' /><br/>
                             </div>
                         </div>
                         <div className="email-contact">
                             <p>Email:</p>
-                            <input required = "Required" type="text" name="email" className="input-contact"/><br/>
+                            <input required = "Required" type="text" name="email" className="input-contact" placeholder='Email Address'/><br/>
                         </div>
                         <div className="feedback-contact">
                             <p>Questions or Feedback</p>
-                            <textarea required = "Required" rows="5" id="message" name="message" className="feedback-box-contact"></textarea><br/>
+                            <textarea required = "Required" rows="5" id="message" name="message" className="feedback-box-contact" placeholder='Type your message here'></textarea><br/>
                         </div>
                         <button type="submit" className="submit-contact-button">Sumbit Form</button>
                     </form>
