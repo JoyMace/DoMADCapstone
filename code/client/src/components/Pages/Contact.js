def372be56e4f8bfc53c4dd6ebf4ed01da618032
Joy Mace <Jocelyn.Mace@colorado.edu>
2020-04-30 21:03:38 -0600

    changes to contact us and register forms

--- code/client/src/components/Pages/Contact.js
+++ code/client/src/components/Pages/Contact.js
@@ -5,33 +5,46 @@ function Contact() {
 
     return (
         <div className="contact">
-            <div className="contact-style">
-                <div className="contact-form-container">
+            <div className="contact-row">
+                <div className='contact-left'>
                     <div className='contact-title'style={{fontSize: '36px'}}>Contact Us</div>
-                    <form action="/api/contact-us/msg/contact" method="POST">
+                    <form className='contact-form' action="/api/contact-us/msg/contact" method="POST">
                         <div className="first-last-name-contact">
                             <div className="first-name-contact">
-                                <p>First Name:</p>
+                                <label>First Name:</label>
                                 <input required = "Required" type="text" name="firstname" className="input-contact" placeholder='First Name'/>
                             </div>
                             <div className="last-name-contact">
-                                <p>Last Name:</p>
+                                <label>Last Name:</label>
                                 <input required = "Required" type="text" name="lastname" className="input-contact" placeholder='Last Name' /><br/>
                             </div>
                         </div>
                         <div className="email-contact">
-                            <p>Email:</p>
+                            <label>Email:</label>
                             <input required = "Required" type="text" name="email" className="input-contact" placeholder='Email Address'/><br/>
                         </div>
                         <div className="feedback-contact">
-                            <p>Questions or Feedback</p>
-                            <textarea required = "Required" rows="5" id="message" name="message" className="feedback-box-contact" placeholder='Type your message here'></textarea><br/>
+                            <label>Questions or Feedback</label>
+                            <textarea required = "Required" rows="9" id="message" name="message" className="feedback-box-contact" placeholder='Type your message here'></textarea><br/>
                         </div>
+                        
                         <button type="submit" className="submit-contact-button">Sumbit Form</button>
                     </form>
                 </div>
+                <div className="contact-right-info">
+              <h1 className="title">We'd love to hear from you!</h1>
+              
+                  <div className="right-info-content">
+                    <br></br>
+                    <h2>Fill out the form to the left to share your thouhgts, concerns, questions or comments.</h2>
+                    <br></br>
+                    <h2>DoMAD will never sell your personal information.</h2>
+                  </div>
+              </div>
             </div>
+            
         </div>
+        
     )
 }
 
