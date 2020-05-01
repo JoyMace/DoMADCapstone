d2a0d05fb7a65092a99e77cc63016ca42435baef
sbkeupp <43832329+sbkeupp@users.noreply.github.com>
2020-04-30 18:13:02 -0600

    Added colon after "travel story" on blogs

--- code/client/src/components/Pages/Blogs.js
+++ code/client/src/components/Pages/Blogs.js
@@ -65,7 +65,7 @@ function BlogEntry(props) { // need to take in props in order to pull from class
                             </div>
                         </IconContext.Provider>
                     </div>
-                    <div className="blog-same-line">Travel Story </div>
+                    <div className="blog-same-line">Travel Story: </div>
                     {props.blog.notes}
                    
                 </div>
